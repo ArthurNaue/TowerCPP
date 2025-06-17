ad85ed10293066517b8fb4ea72b7475fb8e2acff
Arthur Mazzardo Naue <arthur_naue@Hotmail.com>
2025-06-17 17:30:10 -0300

    Git modules

--- build/CMakeFiles/TowerCPP.dir/src/enemy/enemy.cpp.o.d
+++
@@ -1,7 +0,0 @@
-CMakeFiles/TowerCPP.dir/src/enemy/enemy.cpp.o: \
- /home/aluno/Projects/TowerCPP/src/enemy/enemy.cpp \
- /usr/include/stdc-predef.h \
- /home/aluno/Projects/TowerCPP/include/enemy/enemy.hpp \
- /home/aluno/Projects/TowerCPP/include/color_palette/color_palette.hpp \
- /home/aluno/Projects/TowerCPP/libraries/raylib/src/raylib.h \
- /usr/lib/gcc/x86_64-linux-gnu/12/include/stdarg.h
--- build/libraries/raylib/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/aluno/Projects/TowerCPP/libraries/raylib/src
-# Build directory: /home/aluno/Projects/TowerCPP/build/libraries/raylib/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("external/glfw")
